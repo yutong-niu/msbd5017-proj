fe03ec5015ad8d72ff0d50e08dd195ec0fbeaa01
Niu Yutong <jose.niu@hotmail.com>
2022-04-24 00:23:57 +0800

    formatting server.js

--- resolver/server.js
+++ resolver/server.js
@@ -2,40 +2,40 @@ const dns2 = require('dns2');
 const hybridResolve = require('./resolver')
 
 const server = dns2.createServer({
-  udp: true,
-  handle: async (request, send, rinfo) => {
-    const response = await hybridResolve(request); 
-    send(response)
-  }
+    udp: true,
+    handle: async (request, send, rinfo) => {
+        const response = await hybridResolve(request); 
+        send(response)
+    }
 });
 
 server.on('request', (request, response, rinfo) => {
-  console.log(request.header.id, request.questions[0]);
+    console.log(request.header.id, request.questions[0]);
 });
 
 server.on('requestError', (error) => {
-  console.log('Client sent an invalid request', error);
+    console.log('Client sent an invalid request', error);
 });
 
 server.on('listening', () => {
-  console.log(server.addresses());
+    console.log(server.addresses());
 });
 
 server.on('close', () => {
-    console.log('server closed.');
+        console.log('server closed.');
 });
 
 (async() => {
-    const closed = new Promise(resolve => process.on('SIGINT', resolve));
-    await server.listen({
-        udp: { 
-            port: 53,
-            address: "127.0.0.1",
-            type: "udp4",  // IPv4 or IPv6 (Must be either "udp4" or "udp6")
-        },
-    }),
-    console.log('Listening.');
-    await closed;
-    process.stdout.write('\n');
-    await server.close();
+        const closed = new Promise(resolve => process.on('SIGINT', resolve));
+        await server.listen({
+                udp: {
+                        port: 53,
+                        address: "127.0.0.1",
+                        type: "udp4",    // IPv4 or IPv6 (Must be either "udp4" or "udp6")
+                },
+        }),
+        console.log('Listening.');
+        await closed;
+        process.stdout.write('\n');
+        await server.close();
 })();
