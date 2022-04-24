c209e37ad735568eb3dee3d6054e11c1a70a402c
Niu Yutong <yniuaa@connect.ust.hk>
2022-04-25 01:34:49 +0800

    add checks ownership

--- pages/domains/mgmt.js
+++ pages/domains/mgmt.js
@@ -1,49 +1,43 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
 import Domain from '../../ethereum/domain';
+import web3 from '../../ethereum/web3';
 import { Button, Message } from 'semantic-ui-react';
 import { Link } from '../../routes';
 
 class DomainMgmt extends Component {
     static async getInitialProps(props) {
         const { address } = props.query;
-//        const domain = Domain(address);
-//        const isOwner = domain.methods.isOwner().call();
-//        return { address, isOwner};
-        return { address };
+        const domain = Domain(address);
+        const accounts = await web3.eth.getAccounts();
+        const isOwner = await domain.methods.isOwner().call({
+            from: accounts[0]
+        });
+        return { address, isOwner };
     }
-
-//    renderDomain () {
-//        return (<h1>{this.props.address}</h1>);
-//        if (! this.props.isOwner) {
-//            return (
-//                <Message
-//                    error
-//                    header="Permission denied"
-//                    content="You are not the domain owner"
-//                />
-//            );
-//        } else {
-//            return (
-//                <Message
-//                    success
-//                    header="you are the owner"
-//                    content="you are the owner"
-//                />
-//            );
-//        }
-//    }
 
     render () {
         return (
-//            <Layout>
-//                <Link route='/'>
-//                    <a>
-//                        <Button primary>Back</Button>
-//                    </a>
-//                </Link>
-                <h1>{this.props.address}</h1>
-//            </Layout>
+            <Layout>
+                <Link route='/'>
+                    <a>
+                        <Button primary>Back</Button>
+                    </a>
+                </Link>
+                <div>
+                    {(() => {
+                        if (! this.props.isOwner) {
+                            return (
+                                <Message error header="Permission denied" content="You are not the domain owner" />
+                            );
+                        } else {
+                            return (
+                                <Message success header="Permission allowed" content="You are the domain owner" />
+                            )
+                        }
+                    })()} 
+                </div>
+            </Layout>
         );
     }
 }
