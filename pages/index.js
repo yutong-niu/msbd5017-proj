b043e23fd4e748ca252e935dae6bc4459125fea7
Niu Yutong <jose.niu@hotmail.com>
2022-04-25 10:27:31 +0800

    typo

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { Form, Button, Input, Message, Divider } from 'semantic-ui-react';
 import factory from '../ethereum/factory';
 import 'semantic-ui-css/semantic.min.css';
 import Layout from '../components/Layout';
-import { Link, Routes } from '../routes';
+import { Link, Router } from '../routes';
 
 class DomainIndex extends Component {
     state = {
